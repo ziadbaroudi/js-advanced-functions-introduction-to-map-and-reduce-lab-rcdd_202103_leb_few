93b6cddbd689fc90ef244e8c9f49094066412bec
Ziad Baroudi <ziad_baroudi@hotmail.com>
2021-04-27 21:23:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Your code here
+function map(src, callback) {
+  const r = [];
+
+  for (let i = 0; i < src.length; i++) {
+    const theElement = src[i];
+    r.push(callback(theElement));
+  }
+
+  return r;
+}
+
+function reduce(src, callback, startingValue) {
+  let total;
+  if (startingValue) {
+    total = startingValue;
+    for (let i = 0; i < src.length; i++) {
+      total = callback(src[i], total);
+    }
+    return total;
+  } else {
+    total = src[0];
+    for (let i = 1; i < src.length; i++) {
+      total = callback(src[i], total);
+    }
+    return total;
+  }
+}
