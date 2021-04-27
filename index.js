b7a3fb0eb947b0498a5c7a42afd40a9537c1d872
Ziad Baroudi <ziad_baroudi@hotmail.com>
2021-04-27 21:24:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,28 +1,53 @@
-// Your code here
-function map(src, callback) {
-  const r = [];
-
-  for (let i = 0; i < src.length; i++) {
-    const theElement = src[i];
-    r.push(callback(theElement));
+function mapToNegativize(src) {
+  let r = []
+  for (let i = 0; i < src.length; i++ ) {
+    r.push(-1 * src[i])
   }
-
-  return r;
+  return r
 }
 
-function reduce(src, callback, startingValue) {
-  let total;
-  if (startingValue) {
-    total = startingValue;
-    for (let i = 0; i < src.length; i++) {
-      total = callback(src[i], total);
-    }
-    return total;
-  } else {
-    total = src[0];
-    for (let i = 1; i < src.length; i++) {
-      total = callback(src[i], total);
-    }
-    return total;
+function mapToNoChange(src) {
+  let r = []
+  for (let i = 0; i < src.length; i++ ) {
+    r.push(src[i])
   }
+  return r
+}
+
+function mapToDouble(src) {
+  let r = []
+  for (let i = 0; i < src.length; i++ ) {
+    r.push(2 * src[i])
+  }
+  return r
+}
+
+function mapToSquare(src) {
+  let r = []
+  for (let i = 0; i < src.length; i++ ) {
+    r.push(src[i] * src[i])
+  }
+  return r
+}
+
+function reduceToTotal(src, startingPoint=0) {
+  let total = startingPoint
+  for (let i = 0; i < src.length; i++ ) {
+    total = total + src[i]
+  }
+  return total
+}
+
+function reduceToAllTrue(src) {
+  for (let i = 0; i < src.length; i++ ) {
+    if (!src[i]) return false
+  }
+  return true
+}
+
+function reduceToAnyTrue(src) {
+  for (let i = 0; i < src.length; i++ ) {
+    if (src[i]) return true
+  }
+  return false
 }
